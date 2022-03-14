f047d9fa9d4c23974507545a71b2f989f0822e1a
bion <bionjohnson@gmail.com>
2022-03-14 14:03:35 -0700

    Checkout and command delegation

---
+++ bin/lib.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+# This file loads shared code for most shell scripts. All scripts
+# in the base bin/ directory should begin by sourcing it.
+
+pushd $(git rev-parse --show-toplevel) > /dev/null
+
+set -e
+set +x
+
+readonly LIB_DIR="${BASH_SOURCE%/*}/lib"
+
+if [[ "${SOURCED_LIB}" != "true" ]]; then
+  source "${LIB_DIR}/out.sh"
+  source "${LIB_DIR}/snapshots.sh"
+  source "${LIB_DIR}/checkout.sh"
+
+  SOURCED_LIB="true"
+fi
---
+++ bin/lib/checkout.sh
@@ -0,0 +1,118 @@
+#! /usr/bin/env bash
+
+# This file contains shell functions for working with the mainline project
+# checkout. Almost all commands in this deployment repo delegate their
+# implementation to code in the mainline CiviForm repo's cloud/ subtree.
+#
+# Which git revision the scripts should use for that depends on the docker image
+# tag the user specifies in the command.
+
+#######################################
+# Sets the checkout directory to the commit corresponding
+# to the provided image tag.
+# Arguments:
+#   1: The image tag for the server version.
+#######################################
+function checkout::from_image_tag() {
+  local image_tag="${1}"
+
+  if snapshots::tag_is_snapshot "${image_tag}"; then
+    checkout::from_snapshot "${image_tag}"
+  elif [[ "${image_tag}" = "latest" ]]; then
+    printf "Setting checkout to latest... "
+    checkout::at_main > /dev/null
+    echo "done"
+  else
+    out::error "Only SNAPSHOT tags and 'latest' are currently supported."
+    exit 1
+  fi
+}
+
+#######################################
+# Sets the checkout directory to the commit corresponding
+# to the provided snapshot tag.
+# Arguments:
+#   1: The image snapshot tag for the server version.
+#######################################
+function checkout::from_snapshot() {
+  local commit_sha=$(snapshots::get_git_commit_sha "${1}")
+
+  checkout::at_sha "${commit_sha}"
+}
+
+#######################################
+# Checks if the checkout directory is initialized and initializes it
+# if it isn't.
+#######################################
+function checkout::ensure_initialized() {
+  printf "Ensuring checkout is initialized... "
+
+  if cat checkout/.git/info/sparse-checkout 2> /dev/null | grep -q "\/cloud"; then
+    echo "done"
+  else
+    echo "not initialized"
+    checkout::initialize
+  fi
+}
+
+#######################################
+# Initializes the checkout directory by initializing a git repo, setting its
+# origin to the mainline CiviForm repo, and configuring a sparse checkout
+# that only contains the cloud/ directory.
+#######################################
+function checkout::initialize() {
+  printf "Initializing checkout... "
+
+  rm -rf checkout
+  mkdir checkout
+
+  pushd checkout > /dev/null
+
+  git init --initial-branch=main
+  git remote add origin http://github.com/seattle-uat/civiform
+  git config core.sparseCheckout true
+
+  echo "/cloud" >> .git/info/sparse-checkout
+  echo "/bin" >> .git/info/sparse-checkout
+
+  git pull --quiet origin main
+
+  popd > /dev/null
+  echo "done"
+}
+
+#######################################
+# Sets the checkout directory to the commit at the provided SHA.
+# Pulls the latest git revisions first.
+# Arguments:
+#   1: The commit SHA to sync to.
+#######################################
+function checkout::at_sha() {
+  checkout::at_main
+
+  local commit_sha="${1}"
+  printf "Setting checkout to ${commit_sha}... "
+
+  pushd checkout > /dev/null
+
+  git checkout --quiet "${commit_sha}"
+
+  popd > /dev/null
+  echo "done"
+}
+
+#######################################
+# Pulls the latest git revisions for the checkout directory from the mainline
+# CiviForm repo.
+#######################################
+function checkout::at_main() {
+  printf "Syncing checkout directory... "
+
+  pushd checkout > /dev/null
+
+  git checkout --quiet main
+  git pull --quiet origin main
+
+  popd > /dev/null
+  echo "done"
+}
