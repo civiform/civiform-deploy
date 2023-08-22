68dd1139b43c4bcd3112225c2972cc1f085d9bee
Nick Burgan <nick.burgan@exygy.com>
2023-08-22 12:59:00 -0700

    Modify get_cmd_args
    This library's functionality doesn't really need to be wrapped in a function, which is then immediately called. Instead, this sets the default for flags as needed, then directly parses the arguments to find flags and set the corresponding environment variables.

--- bin/lib/get_cmd_args.sh
+++ bin/lib/get_cmd_args.sh
@@ -1,36 +1,21 @@
 #! /usr/bin/env bash
 
 #######################################
-# Retrieves the config file name from a list of arguments if present.
-# Arguments:
-#   @: An arguments list
-# Globals:
-#   Sets the CONFIG variable
+# Retrieves the value of given flags, if present, and
+# sets a default for some variables if they should have one.
 #######################################
-function get_cmd_args::get_config_file() {
-  for i in "$@"; do
-    case "${i}" in
-      --config=*)
-        export CONFIG="${i#*=}"
-        return
-        ;;
-    esac
-  done
 
-  export CONFIG="civiform_config.sh"
-}
+# Defaults
+export CONFIG="civiform_config.sh"
 
-get_cmd_args::get_config_file "$@"
-
-function get_cmd_args::get_force_unlock_id() {
-  for i in "$@"; do
-    case "${i}" in
-      --force-unlock=*)
-        export FORCE_UNLOCK_ID="${i#*=}"
-        return
-        ;;
-      esac
-    done
-}
-
-get_cmd_args::get_force_unlock_id "$@"
+# Parse flags
+for i in "$@"; do
+  case "${i}" in
+    --config=*)
+      export CONFIG="${i#*=}"
+      ;;
+    --force-unlock=*)
+      export FORCE_UNLOCK_ID="${i#*=}"
+      ;;
+  esac
+done
