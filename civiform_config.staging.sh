4439ce64355671946c40e83f743858b9e270b205
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-11-15 13:07:22 -0800

    Set staging version back to latest

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="SNAPSHOT-3ec176a-1700014879"
+export CIVIFORM_VERSION="latest"
 
 # REQUIRED
 # Version of the infrastructure to use.
