840034a36fed7e6391b1f644b4549e05795ab25c
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-02-06 12:48:32 -0800

    Update test civiform versio to v1.50.0

--- civiform_config.test.sh
+++ civiform_config.test.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v1.48.0"
+export CIVIFORM_VERSION="v1.50.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
