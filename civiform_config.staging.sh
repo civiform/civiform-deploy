5d38983fa7e236e6c8bc7fc10b6a479c0b937ed7
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-04-09 17:51:44 -0700

    Temp: Update tests version

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="DEV-d757633-gwen-find-address-multi-urls"
+export CIVIFORM_VERSION="DEV-098cfb6-gwen-find-address-multi-urls"
 
 # REQUIRED
 # Version of the infrastructure to use.
