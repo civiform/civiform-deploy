a71a59c430a0d0824f68e661e8e1c49b215a607a
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-04-12 10:25:41 -0700

    Update prod to v1.59.0

--- civiform_config.prod.sh
+++ civiform_config.prod.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="prod"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v1.57.0"
+export CIVIFORM_VERSION="v1.59.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
@@ -241,9 +241,6 @@ export ADFS_ADMIN_GROUP="f8c8e6c8-a5d7-4476-91bd-7f1691d16625"
 export VERSION_CACHE_ENABLED=true
 export PROGRAM_CACHE_ENABLED=true
 export QUESTION_CACHE_ENABLED=true
-
-# API
-export CIVIFORM_API_KEYS_BAN_GLOBAL_SUBNET=false
 
 # Evolutions
 export DATABASE_APPLY_DESTRUCTIVE_CHANGES=true
