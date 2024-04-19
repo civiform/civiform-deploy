4675bf5e96ceb8e3aca55eb3acf878fc134323ad
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-04-19 12:10:36 -0700

    Testing Play 2

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="latest"
+export CIVIFORM_VERSION="DEV-6a9375d-play-2.9.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
