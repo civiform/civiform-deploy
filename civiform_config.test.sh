fd3e835d239cd15b72547056465a5a5ea24bdbf3
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-06-12 14:24:28 -0700

    Update test to use bug fixed version

--- civiform_config.test.sh
+++ civiform_config.test.sh
@@ -23,7 +23,8 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v2.1.0"
+export CIVIFORM_VERSION="SNAPSHOT-5bd0208-1718226108"
+# export CIVIFORM_VERSION="v2.1.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
@@ -34,8 +35,8 @@ export CIVIFORM_VERSION="v2.1.0"
 #    depending on USE_DOCKER flag.
 #
 # Using "latest" is recommended.
-# export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="latest"
-export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
+export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="latest"
+# export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
 
 # Terraform configuration
 #################################################
