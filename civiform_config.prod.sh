c870a6733f051802d10e8c9f38d309779975f609
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-06-12 17:17:09 -0700

    Set prod to use release with fix in it

--- civiform_config.prod.sh
+++ civiform_config.prod.sh
@@ -23,7 +23,8 @@ export CIVIFORM_MODE="prod"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v2.1.0"
+export CIVIFORM_VERSION="SNAPSHOT-f482ed1-1718235612"
+# export CIVIFORM_VERSION="v2.1.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
@@ -34,7 +35,8 @@ export CIVIFORM_VERSION="v2.1.0"
 #    depending on USE_DOCKER flag.
 #
 # Using "latest" is recommended.
-export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
+export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="latest"
+# export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
 
 # Lock to this postgres version so it doesn't update unexpectedly
 export POSTGRESQL_VERSION="16.2"
