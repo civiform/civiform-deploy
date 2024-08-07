d0ce5068edc2c5476ca7e42135172e84cf2ef8aa
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-08-07 09:13:12 -0700

    Testing branch

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -24,7 +24,7 @@ export CIVIFORM_MODE="staging"
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
 #export CIVIFORM_VERSION="latest"
-export CIVIFORM_VERSION="SNAPSHOT-1df2cb1-1722878128"
+export CIVIFORM_VERSION="DEV-bf32fa2-gwen-jobs"
 
 # REQUIRED
 # Version of the infrastructure to use.
