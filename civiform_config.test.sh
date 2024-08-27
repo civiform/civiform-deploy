cca58cdad75a11195ed4547c015a1180a001b9ac
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-08-27 14:16:20 -0700

    Update test to v2.9.0

--- civiform_config.test.sh
+++ civiform_config.test.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v2.8.0"
+export CIVIFORM_VERSION="v2.9.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
