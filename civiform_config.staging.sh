7b292c120d3078d3fb23e63ea5d33a39177f120f
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-09-17 03:20:02 -0700

    Adding more service area examples

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -256,10 +256,10 @@ export MEASUREMENT_ID="G-HXM0Y35TGE"
 export ESRI_ADDRESS_CORRECTION_ENABLED=true
 export ESRI_FIND_ADDRESS_CANDIDATES_URLS="https://gisdata.seattle.gov/cosgis/rest/services/locators/AddressPoints/GeocodeServer/findAddressCandidates, https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
 export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_ENABLED=true
-export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_URLS="https://gisdata.seattle.gov/server/rest/services/COS/Seattle_City_Limits/MapServer/1/query"
-export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_LABELS="Seattle"
-export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_IDS="Seattle"
-export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_ATTRIBUTES="CITYNAME"
+export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_URLS="https://gisdata.seattle.gov/server/rest/services/COS/Seattle_City_Limits/MapServer/1/query, https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/property__parcel_address_area/MapServer/1722/query, https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/property__parcel_address_area/MapServer/1722/query"
+export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_LABELS="Seattle, IsSmt_PropName, IsSmt_SiteId"
+export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_IDS="Seattle, SEATTLE MUNICIPAL TOWER, {8761A355-5251-49DB-90D3-4B3A885FBF40}"
+export ESRI_ADDRESS_SERVICE_AREA_VALIDATION_ATTRIBUTES="CITYNAME, PROP_NAME, SITEID"
 export ESRI_WELLKNOWN_ID_OVERRIDE="2926"
 
 # Common Intake
