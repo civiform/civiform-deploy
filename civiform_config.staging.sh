5c68419ab59c64bab90cae2f18f50ed71cf850e9
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-12-04 11:40:55 -0800

    Update civiform_config.staging.sh

--- civiform_config.staging.sh
+++ civiform_config.staging.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="staging"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="DEV-7e6b8e6-gwen-enable-mfu-job"
+export CIVIFORM_VERSION="latest"
 
 # REQUIRED
 # Version of the infrastructure to use.
